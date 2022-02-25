7d0372c720359230bb6794be18b7a26f571c8c75
alexandr <kotnyaa@gmail.com>
2022-02-25 11:45:49 +0400

    IS3-5 Add find method

--- src/Domain/Booking/Repository/MovieShowRepository.php
+++ src/Domain/Booking/Repository/MovieShowRepository.php
@@ -9,6 +9,7 @@ use App\Domain\Booking\Entity\ValueObject\Movie;
 use App\Domain\Booking\Entity\ValueObject\Schedule;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Uid\UuidV4;
 
 class MovieShowRepository extends ServiceEntityRepository
 {
@@ -44,10 +45,52 @@ class MovieShowRepository extends ServiceEntityRepository
                     new Hall(
                         $item["hall.numberOfPlaces"]
                     ),
-                ),
+                )
             );
         }
 
         return $moVieShowCollection;
     }
+
+    public function findByUuid(string $id): MovieShow
+    {
+        $entityManager = $this->getEntityManager();
+        $uuid = UuidV4::fromString($id);
+        $queryMovieShow = $entityManager
+            ->createQuery(
+                "SELECT 
+                    ms
+                FROM 
+                    App\Domain\Booking\Entity\MovieShow ms
+                WHERE ms.id = '". $uuid->toRfc4122() . "'"
+            );
+        $queryBuilder = $entityManager->createQueryBuilder();
+        $queryBuilder
+            ->select("ms")
+            ->from("App\Domain\Booking\Entity\MovieShow", "ms")
+            ->where("ms.id = ?1")
+            ->setParameter(1, $uuid->toBinary());
+
+        $movieShow = $queryBuilder->getQuery();
+        $result = $movieShow->getArrayResult();
+
+        return new MovieShow(
+            $result[0]["id"],
+            new Movie(
+                $result[0]["movie.title"],
+                $result[0]["movie.duration"],
+            ),
+            new Schedule(
+                $result[0]["schedule.startAt"],
+                $result[0]["schedule.endAt"],
+            ),
+            new Hall(
+                $result[0]["hall.numberOfPlaces"]
+            ),
+        );
+    }
+
+
+
+
 }
