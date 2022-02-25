579b2f9d0dfb07ead4b26ae5ee51027c3d742fff
alexandr <kotnyaa@gmail.com>
2022-02-25 11:50:09 +0400

    IS3-5 Add send data

--- src/Controller/MovieShowController.php
+++ src/Controller/MovieShowController.php
@@ -5,19 +5,32 @@ namespace App\Controller;
 use App\Domain\Booking\Entity\TransferObject\BookingDto;
 use App\Domain\Booking\Form\BookingType;
 use App\Domain\Booking\Repository\MovieShowRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MovieShowController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
 {
-    #[Route('/', 'booking')]
-    public function index(MovieShowRepository $movieShowRepository): \Symfony\Component\HttpFoundation\Response
-    {
+    #[Route(path: '/', name: 'booking')]
+    public function index(
+        MovieShowRepository $movieShowRepository,
+        Request $request,
+        MessageBusInterface $bus
+    ): \Symfony\Component\HttpFoundation\Response {
         $allMovieShow = $movieShowRepository->findAll();
         foreach ($allMovieShow->getIterator() as $index => $item) {
             $bookingDto = new BookingDto();
             $bookingDto->movieShow = $item->getId();
-            $bookingForm = $this->createForm(BookingType::class, $bookingDto)->createView();
-            $item->setBookingForm($bookingForm);
+            $bookingForm = $this->createForm(BookingType::class, $bookingDto);
+            $bookingFormView = $bookingForm->createView();
+            $item->setBookingForm($bookingFormView);
+            $bookingForm->handleRequest($request);
+            if ($bookingForm->isSubmitted() && $bookingForm->isValid()) {
+                $data = $bookingForm->getData();
+                if ($data->movieShow == $item->getId()) {
+                    $bus->dispatch($data);
+                }
+            }
         }
 
         return $this->render("movieshow.html.twig", ["allMovieShow" => $allMovieShow]);
