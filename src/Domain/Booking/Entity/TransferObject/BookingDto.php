c89d3cc208dcceb3a358ad6103b168c338821471
alexandr <kotnyaa@gmail.com>
2022-02-25 11:44:45 +0400

    IS3-5 Refactor dto

--- src/Domain/Booking/Entity/TransferObject/BookingDto.php
+++ src/Domain/Booking/Entity/TransferObject/BookingDto.php
@@ -7,20 +7,4 @@ class BookingDto
     public string $name;
     public string $phone;
     public string $movieShow;
-
-    public function load(?array $data): void
-    {
-        self::assertCanBeArray($data);
-
-        $this->name = $data["name"];
-        $this->phone = $data["phone"];
-        $this->movieShow = $data["movieShow"];
-    }
-
-    private static function assertCanBeArray(?array $data): void
-    {
-        if (!is_array($data)) {
-            throw new InvalidArgumentException('Error type');
-        }
-    }
 }
