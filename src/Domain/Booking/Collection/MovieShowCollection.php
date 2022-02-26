4d5675cfb7d6b179d7e515ebf7445626eeaf1cbb
alexandr <kotnyaa@gmail.com>
2022-02-26 08:43:13 +0400

    IS3-6 Refactor class

--- src/Domain/Booking/Collection/MovieShowCollection.php
+++ src/Domain/Booking/Collection/MovieShowCollection.php
@@ -4,19 +4,20 @@ namespace App\Domain\Booking\Collection;
 
 use App\Domain\Booking\Entity\MovieShow;
 use Doctrine\Common\Collections\ArrayCollection;
+use DomainException;
 
 class MovieShowCollection extends ArrayCollection
 {
-    public function add(mixed $ticket): void
+    public function add(mixed $movieShow): void
     {
-        self::assertShouldBeMovieShow($ticket);
-        parent::add($ticket);
+        self::assertShouldBeMovieShow($movieShow);
+        parent::add($movieShow);
     }
 
     public static function assertShouldBeMovieShow(mixed $movieShow): void
     {
         if (!$movieShow instanceof MovieShow) {
-            throw new \DomainException('Invalid object');
+            throw new DomainException('Invalid object');
         }
     }
 }
