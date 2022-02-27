0981ec4f015bd58e4bf7ee8d7302b11a5d3b413f
alexandr <kotnyaa@gmail.com>
2022-02-27 23:01:25 +0400

    IS3-6 Fix command

--- src/Domain/Booking/Command/BookingCommand.php
+++ src/Domain/Booking/Command/BookingCommand.php
@@ -7,11 +7,4 @@ class BookingCommand
     public string $name;
     public string $phone;
     public string $movieShow;
-
-    public function load(string $name, string $phone, string $movieShow)
-    {
-        $this->name = $name;
-        $this->phone = $phone;
-        $this->movieShow = $movieShow;
-    }
 }
