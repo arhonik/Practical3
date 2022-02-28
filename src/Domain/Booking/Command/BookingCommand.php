46e29ca514312e6ef032f1460bf9297dd19fe46d
alexandr <kotnyaa@gmail.com>
2022-02-28 13:34:20 +0400

    IS3-6 Add validate attribute

--- src/Domain/Booking/Command/BookingCommand.php
+++ src/Domain/Booking/Command/BookingCommand.php
@@ -2,9 +2,18 @@
 
 namespace App\Domain\Booking\Command;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class BookingCommand
 {
-    public string $name;
-    public string $phone;
+    #[Assert\Uuid]
+    #[Assert\NotBlank]
     public string $movieShowId;
+
+    #[Assert\NotBlank]
+    public string $name;
+
+    #[Assert\NotBlank]
+    #[Assert\Regex('/^(\+7|8)\d{10}$/')]
+    public string $phone;
 }
