6b51738296422cbd121262abdca037697da112d0
alexandr <kotnyaa@gmail.com>
2022-03-04 21:26:59 +0400

    IS4-1 Refactor test TicketsCollection

--- tests/Collection/TicketsCollectionTest.php
+++ tests/Collection/TicketsCollectionTest.php
@@ -3,54 +3,39 @@
 namespace App\Tests\Collection;
 
 use App\Domain\Booking\Collection\TicketsCollection;
-use App\Domain\Booking\Entity\MovieShow;
 use App\Domain\Booking\Entity\Ticket;
-use App\Domain\Booking\Entity\ValueObject\Customer;
-use DateTimeImmutable;
-use DateTimeZone;
 use DomainException;
 use Monolog\Test\TestCase;
-use Symfony\Component\Uid\Uuid;
+use stdClass;
 
 class TicketsCollectionTest extends TestCase
 {
-    private Ticket $ticket;
     private TicketsCollection $ticketsCollection;
 
     protected function setUp(): void
     {
         parent::setUp();
-
-        $this->ticket = new Ticket(
-            Uuid::v4(),
-            $this->createMock(MovieShow::class),
-            new Customer(
-                'Alex',
-                '+79021869474'
-            ),
-            'Venom 2',
-            DateTimeImmutable::createFromFormat(
-                'Y-m-d H:i',
-                '2022-10-11 19:45',
-                new DateTimeZone('Europe/Moscow')
-            )
-        );
         $this->ticketsCollection = new TicketsCollection();
     }
 
     public function testExceptionAddTicket(): void
     {
-        $movieShow = $this->createMock(MovieShow::class);
+        $object = new stdClass();
 
         $this->expectException(DomainException::class);
 
-        $this->ticketsCollection->add($movieShow);
+        $this->ticketsCollection->add($object);
     }
 
     public function testCorrectAddTicket(): void
     {
-        $this->ticketsCollection->add($this->ticket);
+        $this->ticketsCollection->add($this->getTicket());
 
         $this->assertCount(1, $this->ticketsCollection);
+    }
+
+    private function getTicket(): Ticket
+    {
+        return $this->createMock(Ticket::class);
     }
 }
