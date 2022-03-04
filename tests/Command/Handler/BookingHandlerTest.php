cad1c6693f4861bc1be22169c3507f4d9cc6865f
alexandr <kotnyaa@gmail.com>
2022-03-04 11:12:09 +0400

    IS4-1 Add test BookingHandler

---
+++ tests/Command/Handler/BookingHandlerTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Tests\Command\Handler;
+
+use App\Domain\Booking\Command\BookTicketCommand;
+use App\Domain\Booking\Entity\MovieShow;
+use App\Domain\Booking\Repository\MovieShowRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Uid\Uuid;
+
+class BookingHandlerTest extends WebTestCase
+{
+    protected ?MessageBusInterface $bus;
+    protected ?MovieShowRepository $movieShowRepository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->bus = self::getContainer()->get(MessageBusInterface::class);
+
+        $this->movieShowRepository = self::getContainer()->get(MovieShowRepository::class);
+    }
+
+    public function testExecutionCommand(): void
+    {
+        $movieShow = $this->getFirstMovieShow();
+        $numberOfFreePlaces = $movieShow->getNumberOfAvailablePlacesForBooking();
+        $command = $this->getCommand($movieShow->getId());
+        $this->bus->dispatch($command);
+        $movieShowExpect = $this->getFirstMovieShow();
+
+        self::assertNotEquals(
+            $numberOfFreePlaces,
+            $movieShowExpect->getNumberOfAvailablePlacesForBooking()
+        );
+    }
+
+    private function getFirstMovieShow(): MovieShow
+    {
+        $movieShowCollection = $this->movieShowRepository->findAll();;
+
+        $movieShow = $movieShowCollection->get(0);
+        $movieShowId = $movieShow->getId();
+
+        return $this->movieShowRepository->findById($movieShowId);
+    }
+
+    private function getCommand(Uuid $movieShowId): BookTicketCommand
+    {
+        $command = new BookTicketCommand();
+        $command->phone = '+79021869474';
+        $command->name = 'Alex';
+        $command->movieShowId = $movieShowId->toRfc4122();
+
+        return $command;
+    }
+}
